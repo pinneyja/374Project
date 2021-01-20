437d3124b85eedfddfbb08ae26b37f7063b2252a
pinneyja <pinneyja@rose-hulman.edu>
2021-01-20 11:06:46 -0500

    restructured into src folder, added classes in service layer

---
+++ src/ServiceLayer/Order.java
@@ -0,0 +1,13 @@
+public class Order {
+    private int orderID, zipCode;
+    private String streetAddress, drinkName;
+    ArrayList<Option> options;
+
+    Order(int orderID, String streetAddress, int zipCode, String drinkName, ArrayList<Option> options) {
+        this.orderID = orderID;
+        this.streetAddress = streetAddress;
+        this.zipCode = zipCode;
+        this.drinkName = drinkName;
+        this.options = options;
+    }
+}
