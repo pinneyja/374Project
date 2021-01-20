b116b0fa99ec8d62f74cf46c992dc0998ad97ed0
pinneyja <pinneyja@rose-hulman.edu>
2021-01-20 16:56:01 -0500

    moved JSON keys to top of class, added logic to catch potential misformatting of orders

--- src/ServiceLayer/ApplicationInterface.java
+++ src/ServiceLayer/ApplicationInterface.java
@@ -9,6 +9,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ApplicationInterface extends Publisher {
+    private static final String ORDER_KEY = "order";
+    private static final String ORDER_ID_KEY = "orderID";
+    private static final String ADDRESS_KEY = "address";
+    private static final String STREET_KEY = "street";
+    private static final String ZIP_KEY = "ZIP";
+    private static final String DRINK_NAME_KEY = "drink";
+    private static final String CONDIMENTS_KEY = "condiments";
+    private static final String CONDIMENT_NAME_KEY = "name";
+    private static final String CONDIMENT_QTY_KEY = "qty";
+
     HashMap<Integer, Order> orders;
 
     public ApplicationInterface() {
@@ -17,31 +27,36 @@ public class ApplicationInterface extends Publisher {
     }
 
     public void placeOrder(String jsonOrder) {
-        Order newOrder = parseOrder(jsonOrder);
-        orders.put(newOrder.getOrderID(), newOrder);
-        eventChannel.notifySubscribers(newOrder);
+        try {
+            Order newOrder = parseOrder(jsonOrder);
+            orders.put(newOrder.getOrderID(), newOrder);
+            eventChannel.notifySubscribers(newOrder);
+        } catch (JSONException exception) {
+            System.out.println("Oops! Something was formatted incorrectly in the order:\"\n" + jsonOrder + "\n\". " +
+                    "It caused the following error: \"" + exception.getMessage() + "\" Please try again!");
+        }
     }
 
-    private Order parseOrder(String jsonOrderAsString) {
-        JSONObject jsonOrder = new JSONObject(jsonOrderAsString).getJSONObject("order");
+    private Order parseOrder(String jsonOrderAsString) throws JSONException {
+        JSONObject jsonOrder = new JSONObject(jsonOrderAsString).getJSONObject(ORDER_KEY);
 
-        int orderID = jsonOrder.getInt("orderID");
+        int orderID = jsonOrder.getInt(ORDER_ID_KEY);
 
-        JSONObject jsonAddress = jsonOrder.getJSONObject("address");
-        String streetAddress = jsonAddress.getString("street");
-        int zipCode = jsonAddress.getInt("ZIP");
+        JSONObject jsonAddress = jsonOrder.getJSONObject(ADDRESS_KEY);
+        String streetAddress = jsonAddress.getString(STREET_KEY);
+        int zipCode = jsonAddress.getInt(ZIP_KEY);
 
-        String drinkName = jsonOrder.getString("drink");
+        String drinkName = jsonOrder.getString(DRINK_NAME_KEY);
 
         JSONArray condiments;
         ArrayList<Option> orderOptions = null;
         try {
-            condiments = jsonOrder.getJSONArray("condiments");
+            condiments = jsonOrder.getJSONArray(CONDIMENTS_KEY);
             orderOptions = new ArrayList<>();
             for(int i = 0; i < condiments.length(); i ++) {
                 JSONObject jsonOption = condiments.getJSONObject(i);
-                String optionName = jsonOption.getString("name");
-                int optionQuantity = jsonOption.getInt("qty");
+                String optionName = jsonOption.getString(CONDIMENT_NAME_KEY);
+                int optionQuantity = jsonOption.getInt(CONDIMENT_QTY_KEY);
 
                 Option option = new Option(optionName, optionQuantity);
                 orderOptions.add(option);
