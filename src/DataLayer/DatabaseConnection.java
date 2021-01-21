8e5ebb86b3f908eb3393e2daabf96afa273731ae
bohnernj <bohnernj@rose-hulman.edu>
2021-01-21 10:15:16 -0500

    Made changes

--- src/DataLayer/DatabaseConnection.java
+++ src/DataLayer/DatabaseConnection.java
@@ -1,15 +1,16 @@
 package DataLayer;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
+import BusinessLayer.AppResponse;
+import BusinessLayer.CoffeeMaker;
+import BusinessLayer.Option;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
  
 
 public class DatabaseConnection {
@@ -20,41 +21,44 @@ public class DatabaseConnection {
     // public DatabaseConnection(Database db) {
     // }
 
-    public ArrayList<CoffeeMachine> getCoffeeMachinesAtAddress(String address, int zipCode) {
+public ArrayList<CoffeeMachine> getCoffeeMachinesAtAddress(String address, int zipCode){
         //So, ideal return would be {[coffeeMachID:"1", type:""],[coffeeMachID:"2", type:""]}
   
         //JSON parser object to parse read file
 
-        JSONParser jsonParser = new JSONParser();
-        List<CoffeeMachine> controllerList = new ArrayList<CoffeeMachine>();
+        List<CoffeeMachine> controllerList = new ArrayList<CoffeeMachine>(); 
 
-        try (FileReader reader = new FileReader("controllers.json"))
-        {
-            //Read JSON file
-            Object obj = jsonParser.parse(reader);
- 
-            JSONArray controllers = (JSONArray) obj;
-            System.out.println(controllers);
-             
-            //Iterate over con array
-            controllers.forEach( con -> {
-                        JSONObject controller = (JSONObject) con.get("controller");
+        String str= createControllerResponseString("controllers.json");
+        ArrayList<CoffeeMachine> machines= parseCM(str);
 
-                        if(controller.get("StreetAddress").equals(address)){
-                            CoffeeMachine currentMachine = new CoffeeMachine(controller.get("coffee_machine_id") ,controller.get("ControllerID"), controller.get("Type"));
-                            controllerList.add(currentMachine);
-                        }
-                    });
+        return machines;
+    }
+	public String createControllerResponseString(File controllerResponseFile) throws FileNotFoundException {
+		FileReader reader = new FileReader(controllerResponseFile);
+		StringBuilder sb = new StringBuilder();
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
+		try {
+			while (reader.read() != -1) {
+				sb.append(reader.read());
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String jsonCRAsString = sb.toString();
+		return jsonCRAsString;
+	}
+
+    public ArrayList<CoffeeMachine> parseCM(String str){
+        ArrayList controllerList= new ArrayList<CoffeeMachine>();
+
+        JSONArray something = new JSONArray(str);
+        for(int i; i<something.length(); i++){
+            JSONObject j=something.getJSONObject(i);
+            CoffeeMachine currentMachine = new CoffeeMachine(j.getInt("coffee_machine_id") ,j.getInt("ControllerID"), j.getString("Type"));
+            controllerList.add(currentMachine);
+            
         }
-
-
         return controllerList;
     }
 
