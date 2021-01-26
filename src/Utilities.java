9e2438bd5e99ebc898c679ca2b5b6ed2e34a3349
pinneyja <pinneyja@rose-hulman.edu>
2021-01-26 11:24:42 -0500

    remove print statement, ignore files in out folder for git

--- src/Utilities.java
+++ src/Utilities.java
@@ -11,7 +11,7 @@ public class Utilities {
     public static void writeStringToLocalFile(String localResourceFileName, String fileData) {
         String dir = Paths.get("").toAbsolutePath().getParent().toString();
         String localPath = dir + "\\resources\\out\\" + localResourceFileName;
-        
+
         try {
             FileWriter fileWriter = new FileWriter(localPath);
             fileWriter.write(fileData);
